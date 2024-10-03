a4b8fea36a3a7572e6f37444099978b631dec5f0
Olga Petrova <mascee@gmail.com>
2024-10-03 13:42:42 -0700

    Added more analysis

--- EmployeeSQL/EmployeeSQL_QuickDBD.sql
+++ EmployeeSQL/EmployeeSQL_QuickDBD.sql
@@ -65,4 +65,68 @@ CREATE TABLE "salaries" (
 );
 SELECT * FROM salaries;
 
+--List the employee number, last name, first name, sex, and salary of each employee.
+SELECT 
+    employee.emp_no,
+    employee.last_name,
+    employee.first_name,
+    employee.sex,
+    salary.salary
+FROM 
+    employees AS employee
+JOIN 
+    salaries AS salary ON employee.emp_no = salary.emp_no;
+	
+--List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT 
+    first_name,
+    last_name,
+    hire_date
+FROM 
+    employees
+WHERE 
+    EXTRACT(YEAR FROM hire_date) = 1986;
+--List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT 
+    departments.dept_no,
+    departments.dept_name,
+    dept_manager.emp_no,
+    employees.last_name,
+    employees.first_name
+FROM 
+    dept_manager
+JOIN 
+    departments ON dept_manager.dept_no = departments.dept_no
+JOIN 
+    employees ON dept_manager.emp_no = employees.emp_no;
+	
+--List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT 
+    dept_emp.dept_no,
+    employees.emp_no,
+    employees.last_name,
+    employees.first_name,
+    departments.dept_name
+FROM 
+    dept_emp
+JOIN 
+    employees ON dept_emp.emp_no = employees.emp_no
+JOIN 
+    departments ON dept_emp.dept_no = departments.dept_no;
+	
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT 
+    first_name,
+    last_name,
+    sex
+FROM 
+    employees
+WHERE 
+    first_name = 'Hercules' 
+    AND last_name LIKE 'B%';
 
+--List each employee in the Sales department, including their employee number, last name, and first name.
+
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+--List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
